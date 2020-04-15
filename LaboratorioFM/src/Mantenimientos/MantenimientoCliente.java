3dd40acdb40285481fef2295115d4a9302990a20
Diana-Victores <dcpernillo@gmail.com>
2020-04-14 22:42:01 -0600

    mod en mantenimientos

--- LaboratorioFM/src/Mantenimientos/MantenimientoCliente.java
+++ LaboratorioFM/src/Mantenimientos/MantenimientoCliente.java
@@ -214,6 +214,7 @@ public class MantenimientoCliente extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         System.exit(0);
+        
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
